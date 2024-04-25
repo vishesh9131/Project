e071183de0708aedc694ca9b01ca202235e97fd3
Vishesh <87526302+vishesh9131@users.noreply.github.com>
2024-04-25 10:24:16 +0530

    update 1.1

--- app.py
+++ app.py
@@ -9,11 +9,46 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn
 
-page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+# page = st.selectbox("Explore Or Predict", ("Predict", "Explore"))
+# page = st.title("Predict")
 
-if page == "Predict":
-    show_predict_page()
+# if page == "Predict":
+show_predict_page()
 # else:
 #     show_explore_page()
 
 
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            .viewerBadge_container__1QSob {display: none;}
+            .element-container a[target="_blank"] {
+                display: none !important;
+            }
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
+# Load your dataset (replace 'your_dataset.csv' with the path to your dataset)
+df = pd.read_csv('survey_results_public.csv')
+
+
+st.markdown("---")
+
+# Display the dataset using st.dataframe()
+st.write("## Dataset feeded to the model.")
+st.dataframe(df)
+
+# Add a horizontal rule to separate content
+st.markdown("---")
+
+# Define the code credit section
+code_credit_section = """
+### Code Credits
+This Streamlit app was created by Vishesh Yadav.
+"""
+
+# Display the code credit section
+st.markdown(code_credit_section)
